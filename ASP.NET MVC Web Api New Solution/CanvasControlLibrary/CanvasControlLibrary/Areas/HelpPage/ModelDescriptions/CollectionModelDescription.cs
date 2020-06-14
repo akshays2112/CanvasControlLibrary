70571e42d9379cc52a8f6a0a86a7fce308ca36bb
Akshay Srinivasan <35820570+akshays2112@users.noreply.github.com>
2020-06-14 13:03:28 +0530

    Upgraded all Packages to current.  GitHub dependencybot informed me that there was a security problem with the version of bootstrap.

---
+++ ASP.NET MVC Web Api New Solution/CanvasControlLibrary/CanvasControlLibrary/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace CanvasControlLibrary.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ASP.NET MVC Web Api New Solution/CanvasControlLibrary/CanvasControlLibrary/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CanvasControlLibrary.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
